9660a5f2a6ce08bbc6c7b1e02f40eb1840063aa3
hieuhieu2202 <66201835+hieuhieu2202@users.noreply.github.com>
2025-10-15 16:56:25 +0700

    Register HomeRepository with GetX bindings

--- lib/app/modules/home/home_binding.dart
+++ lib/app/modules/home/home_binding.dart
@@ -1,10 +1,12 @@
 
 import 'package:get/get.dart';
+import 'package:myapp/app/data/repositories/home_repository.dart';
 import 'package:myapp/app/modules/home/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<HomeRepository>(() => HomeRepository());
     Get.lazyPut<HomeController>(() => HomeController());
   }
 }
